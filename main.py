c66f4a5bc33311eff015de7af87569d8e9c0efca
fabio-biot <chaput.fabio@gmail.com>
2026-04-01 10:42:31 +0200

    matrice

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from enum import Enum
+import random
 
 line1 = "█"
 print(line1)
+
 
 class Direction(Enum):
     N = (0, -1)
@@ -51,6 +53,11 @@ class MazeGenerator:
 
         return neighbors
 
+    def get_unvisited_neighbors(self, x: int, y: int):
+        neighbors = self.get_neighbors(x, y)
+        return [(direction, nx, ny) for direction, nx, ny in neighbors
+                if not self.get_cell(nx, ny).visited]
+
     def remove_wall(self, x, y, nx, ny, direction: Direction):
         self.grid[y][x].walls[direction] = False
         self.grid[ny][nx].walls[OPPOSITE[direction]] = False
@@ -58,16 +65,65 @@ class MazeGenerator:
     def get_cell(self, x, y):
         return self.grid[y][x]
 
+    def generate(self):
+        stack = []
+        # i = 0
+        x, y = 0, 0
+        self.get_cell(x, y).visited = True
+        stack.append((x, y))
+
+        while stack: # i != 5
+            print(stack)
+            x, y = stack[-1]
+            neighbors = self.get_unvisited_neighbors(x, y)
+
+            if neighbors:
+                direction, nx, ny = random.choice(neighbors)
+                self.remove_wall(x, y, nx, ny, direction)
+            
+                self.get_cell(nx, ny).visited = True
+                stack.append((nx, ny))
+            else:
+                stack.pop()
+            # i += 1
+        return stack
+    
+    def display(self):
+        print("█" + "████" * self.width)
+
+        for y in range(self.height):
+            line1 = "█"
+            line2 = "█"
+
+            for x in range(self.width):
+                cell = self.get_cell(x, y)
+                line1 += "   "
+                if cell.walls[Direction.E]:
+                    line1 += "█"
+                else:
+                    line1 += " "
+                if cell.walls[Direction.S]:
+                    line2 += "████"
+                else:
+                    line2 += "   █"
+
+            print(line1)
+            print(line2)
+
 
 def main():
     maze = MazeGenerator(10, 5)
 
     print(maze.width, maze.height)
-
+    maze.display()
+    stack = maze.generate()
     for row in maze.grid:
         for cell in row:
             print("█", end="")
         print()
+    print(stack)
+    maze.display()
+    print("SIU")
 
 
 if __name__ == "__main__":
